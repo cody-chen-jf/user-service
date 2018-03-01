0e2fb81a9bbbfb22789146390aa2ffada2f7582f
cody-chen-jf <459741515@qq.com>
2018-03-01 23:34:17 +0800

    update for test

--- src/main/java/com/example/user/controller/UserController.java
+++ src/main/java/com/example/user/controller/UserController.java
@@ -1,15 +1,24 @@
 package com.example.user.controller;
 
 import com.example.user.common.RestResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
+    @Value("${server.port}")
+    private String port;
+
     @RequestMapping("/getUserName")
     public RestResponse<String> getUserName(Long id){
         return RestResponse.success("test-user");
+    }
+
+    @RequestMapping("/getUserServicePort")
+    public RestResponse<String> getUserServicePort(){
+        return RestResponse.success(port);
     }
 
 }
